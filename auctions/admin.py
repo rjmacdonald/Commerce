3b19889f7f6a8bc24f2275efed8bcaf21864400a
Ryan Macdonald <Ryan@Ryans-MBP-4.lan>
2020-08-20 16:12:53 +0100

    Admin configuration

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . import models
+
+class UserAdmin (admin.ModelAdmin):
+    list_display = ("id", "username", "first_name", "last_name", "email", "is_superuser", "is_staff", "is_active")
 
 # Register your models here.
+admin.site.register(models.User, UserAdmin)

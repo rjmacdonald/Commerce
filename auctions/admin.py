c598c7c0f76a7ec0e3ff94d8ac15cc6fa4e9e99c
Ryan Macdonald <Ryan@Ryans-MBP-4.lan>
2020-08-22 21:53:38 +0100

    admin display config

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,12 +1,24 @@
 from django.contrib import admin
 from . import models
 
-class UserAdmin (admin.ModelAdmin):
+class UserAdmin(admin.ModelAdmin):
     list_display = ("id", "username", "first_name", "last_name", "email", "is_superuser", "is_staff", "is_active")
+
+class BidAdmin(admin.ModelAdmin):
+    list_display = ("id", "listing", "user", "bid_amount", "timestamp", "is_winner")
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "category")
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("id", "listing", "user", "comment", "timestamp")
+
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ("id", "owner", "title", "starting_bid", "category", "timestamp", "active")
 
 # Register your models here.
 admin.site.register(models.User, UserAdmin)
-admin.site.register(models.Bid)
-admin.site.register(models.Category)
-admin.site.register(models.Comment)
-admin.site.register(models.Listing)
+admin.site.register(models.Bid, BidAdmin)
+admin.site.register(models.Category, CategoryAdmin)
+admin.site.register(models.Comment, CommentAdmin)
+admin.site.register(models.Listing, ListingAdmin)
